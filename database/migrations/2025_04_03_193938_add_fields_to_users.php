20a09ed3c238a32077d0b98b8ebaa35b71387055
juanframart2011 <juanframart2011@gmail.com>
2025-04-03 13:55:18 -0600

    migraciones

---
+++ database/migrations/2025_04_03_193938_add_fields_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable()->after('id'); // Relación con roles
+            $table->softDeletes(); // deleted_at para softDeletes
+
+            // Clave foránea para role_id
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']); // Eliminar la clave foránea
+            $table->dropColumn(['role_id', 'deleted_at']); // Eliminar las columnas
+        });
+    }
+};
